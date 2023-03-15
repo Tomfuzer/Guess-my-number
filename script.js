fd5a558fed2208b79c4db7f30b640eea6d70cf39
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-03-15 14:43:44 -0300

    Envio dos arquivos da aplicação

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+//Autor: Hamilton Fuzer de Oliveira
+
+function gerador() {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  return secretNumber;
+}
+let secretNumber = gerador();
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+//addEventListener primeiro parametro: qual eh o evento; segundo parametro: o que fazer quando o evento acontecer (function)
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  //no input
+  if (!guess) {
+    displayMessage('⛔ NO NUMBER');
+    //Player win
+  } else if (guess === secretNumber) {
+    displayMessage('🥳 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+    //Guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage(' YOU LOST THE GAME');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Reset the game 'Again!'
+document.querySelector('.again').addEventListener('click', function () {
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  secretNumber = gerador();
+});
